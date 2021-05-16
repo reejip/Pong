9b7b3636d9c07e1b32035eb6bd8fc42a07d39e69
reejip <83128491+reejip@users.noreply.github.com>
2021-05-16 14:43:00 +0200

    arkanoid

---
+++ pong/Assets/Arkanoid/Controls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{   
+    public float speed  = 150 ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+        void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
