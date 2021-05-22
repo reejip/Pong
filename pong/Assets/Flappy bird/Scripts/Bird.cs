9b4c332b984b4a0903db0ac6872c86765a35792d
reejip <83128491+reejip@users.noreply.github.com>
2021-05-22 16:24:08 +0200

    nn

---
+++ pong/Assets/Flappy bird/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 5f;
+
+    [SerializeField]
+
+    Rigidbody2D rb2b;
+    private float flapForce = 20f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2b = GetComponent<Rigidbody2D>();
+
+        rb2b.velocity = Vector2.right * speed * Time.deltaTime; 
+
+            
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            rb2b.velocity = Vector2.right * speed * Time.deltaTime;
+
+        rb2b.AddForce(Vector2.up * flapForce);
+    }
+}
