8ce819123e8f2c9d55f38d024b146f96a37f9de3
reejip <83128491+reejip@users.noreply.github.com>
2021-06-18 14:09:29 +0200

    eee

--- pong/Assets/Flappy/scipts/Bird.cs
+++ pong/Assets/Flappy/scipts/Bird.cs
@@ -1,33 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Bird : MonoBehaviour
 {
     Rigidbody2D rb2d;
     public float speed = 5f;
-
+    bool isDead;
     [SerializeField]
     private float flapForce = 20f;
     // Start is called before the first frame update
     void Start()
     {
+        
+
+        Time.timeScale = 0;
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = Vector2.right * speed * Time.deltaTime;
-
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) &&  !isDead)
         {
             rb2d.velocity = Vector2.right * speed * Time.deltaTime;
             rb2d.AddForce(Vector2.up * flapForce);
         }
-
-
-
     }
+    public GameObject ReplayButton;
+    void OnCollisionEnter2D(Collision2D col)
+        {
+            isDead = true;
+            rb2d.velocity = Vector2.zero;
+            ReplayButton.SetActive(true);
+            GetComponent<Animator>().SetBool("isDead" , true);
+            Debug.Log("collided");
+        }
+   public void Replay()
+   {
+          SceneManager.LoadScene(0);
+   }
+   public void UnFreeze()
+   {
+             Time.timeScale = 1;
+
+   }
+
+   public void BtnOnClick(){
+   gameObject.GetComponent<Button>().interactable = false;
+   }  
+   
 }
---
+++ pong/Assets/Flappy/scipts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+public float minY;
+public float maxY;
+public float distance;
+void OnTriggerEnter2D(Collider2D coll)
+{
+    if(coll.tag == "Obstacle")
+    {
+
+    float obstacleY = Random.Range(minY, maxY);
+    Vector3 spawnPosition = new Vector3(transform.position.x + distance, obstacleY, 0);
+    coll.gameObject.transform.position = spawnPosition;
+    }
+}
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
